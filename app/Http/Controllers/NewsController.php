58a530d24be39bc01a4d712ba40f33cbc587c925
Natalcr <walfxstar@gmail.com>
2025-12-14 09:29:28 +0700

    Add files via upload

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        $news = News::latest()->get();
+        return view('news.index', compact('news'));
+    }
+
+    public function create()
+    {
+        return view('news.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        News::create($request->all());
+        return redirect()->route('news.index');
+    }
+
+    public function show(News $news)
+    {
+        return view('news.show', compact('news'));
+    }
+
+    public function edit(News $news)
+    {
+        return view('news.edit', compact('news'));
+    }
+
+    public function update(Request $request, News $news)
+    {
+        $news->update($request->all());
+        return redirect()->route('news.index');
+    }
+
+    public function destroy(News $news)
+    {
+        $news->delete();
+        return redirect()->route('news.index');
+    }
+}
